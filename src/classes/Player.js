617d8009c4a68946fafb1ef5a72d3d9bf3b992fa
Lara Scremin <lscreminmendes@gmail.com>
2025-03-19 22:30:30 -0300

    movement logic

---
+++ src/classes/Player.js
@@ -0,0 +1,26 @@
+class Player {
+  constructor(canvasWidth, canvasHeight) {
+    this.width = 100;
+    this.height = 100;
+    this.velocity = 6;
+    this.position = {
+      x: canvasWidth / 2 - this.width / 2,
+      y: canvasHeight - this.height - 50,
+    };
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  moveLeft() {
+    player.position.x -= this.velocity;
+  }
+
+  moveRight() {
+    player.position.x += this.velocity;
+  }
+}
+
+export default Player;
