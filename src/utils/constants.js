630042624ea78f42d7e370a33230f5cacf28118d
Lara Scremin <lscreminmendes@gmail.com>
2025-03-21 21:14:18 -0300

    constant path to pngs

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const PATH_SPACESHIP_IMG = "src/assets/images/spaceship.png";
+export const PATH_ENGINE_IMG = "src/assets/images/engine.png";
+export const PATH_SSPRITES_IMG = "src/assets/images/sprites.png";
