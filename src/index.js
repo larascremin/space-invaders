8cd19ccdee63492d0036d6276bf546d82f4d89f8
Lara Scremin <lscreminmendes@gmail.com>
2025-04-05 15:46:30 -0300

    add functions and game state controls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import Grid from "./classes/Grid.js";
 import Invader from "./classes/Invader.js";
+import Obstacle from "./classes/Obstacle.js";
+import Particle from "./classes/Particle.js";
 import Player from "./classes/Player.js";
 import Projectile from "./classes/Projectile.js";
+import { GameState } from "./utils/constants.js";
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -11,10 +14,29 @@ canvas.height = innerHeight;
 
 ctx.imageSmoothingEnabled = false;
 
+let currentState = GameState.PLAYING;
+
 const player = new Player(canvas.width, canvas.height);
 const grid = new Grid(3, 6);
 const playerProjectiles = [];
 const invaderProjectiles = [];
+const particles = [];
+const obstacles = [];
+
+const initObstacles = () => {
+  const x = canvas.width / 2 - 50;
+  const y = canvas.height / 2 - 250;
+  const offset = canvas.width * 0.15;
+  const color = "crimson";
+
+  const obst1 = new Obstacle({ x: x - offset, y }, 100, 20, color);
+  const obst2 = new Obstacle({ x: x + offset, y }, 100, 20, color);
+
+  obstacles.push(obst1);
+  obstacles.push(obst2);
+};
+
+initObstacles();
 
 const keys = {
   left: false,
@@ -23,6 +45,10 @@ const keys = {
     pressed: false,
     released: true,
   },
+};
+
+const drawObstacles = () => {
+  obstacles.forEach((obstacle) => obstacle.draw(ctx));
 };
 
 const drawProjectiles = () => {
@@ -34,6 +60,13 @@ const drawProjectiles = () => {
   });
 };
 
+const drawParticles = () => {
+  particles.forEach((particle) => {
+    particle.draw(ctx);
+    particle.update();
+  });
+};
+
 const clearProjectiles = () => {
   playerProjectiles.forEach((projectile, index) => {
     if (projectile.position.y <= 0) {
@@ -42,43 +75,180 @@ const clearProjectiles = () => {
   });
 };
 
+const clearParticles = () => {
+  particles.forEach((particle, i) => {
+    if (particle.opacity <= 0) {
+      particles.splice(i, 1);
+    }
+  });
+};
+
+const createExplosion = (position, size, color) => {
+  for (let i = 0; i < size; i += 1) {
+    const particle = new Particle(
+      {
+        x: position.x,
+        y: position.y,
+      },
+      {
+        x: Math.random() - 0.5 * 2,
+        y: Math.random() - 0.5 * 2,
+      },
+      2,
+      color
+    );
+
+    particles.push(particle);
+  }
+};
+
+const shootPlayer = () => {
+  invaderProjectiles.some((projectile, projectileIndex) => {
+    if (player.gotHit(projectile)) {
+      invaderProjectiles.splice(projectileIndex, 1);
+      gameOver();
+    }
+  });
+};
+
+const shootInvaders = () => {
+  grid.invadersGrid.forEach((invader, invaderIndex) => {
+    playerProjectiles.some((projectile, projectileIndex) => {
+      if (invader.gotHit(projectile)) {
+        createExplosion(
+          {
+            x: invader.position.x + invader.width / 2,
+            y: invader.position.y + invader.height / 2,
+          },
+          10,
+          "#941CFF"
+        );
+        grid.invadersGrid.splice(invaderIndex, 1);
+        playerProjectiles.splice(projectileIndex, 1);
+      }
+    });
+  });
+};
+
+const shootObstacle = () => {
+  obstacles.forEach((obstacle) => {
+    playerProjectiles.some((projectile, projectileIndex) => {
+      if (obstacle.gotHit(projectile)) {
+        playerProjectiles.splice(projectileIndex, 1);
+      }
+    });
+
+    invaderProjectiles.some((projectile, projectileIndex) => {
+      if (obstacle.gotHit(projectile)) {
+        invaderProjectiles.splice(projectileIndex, 1);
+      }
+    });
+  });
+};
+
+const spawnGrid = () => {
+  if (grid.invadersGrid.length === 0) {
+    grid.rows = Math.round(Math.random * 9 + 1);
+    grid.colums = Math.round(Math.random * 9 + 1);
+    grid.restart();
+  }
+};
+
+const gameOver = () => {
+  createExplosion(
+    {
+      x: player.position.x + player.width / 2,
+      y: player.position.y + player.height / 2,
+    },
+    10,
+    "white"
+  );
+
+  createExplosion(
+    {
+      x: player.position.x + player.width / 2,
+      y: player.position.y + player.height / 2,
+    },
+    10,
+    "#4D9BE6"
+  );
+
+  createExplosion(
+    {
+      x: player.position.x + player.width / 2,
+      y: player.position.y + player.height / 2,
+    },
+    10,
+    "crimson"
+  );
+
+  currentState = GameState.GAME_OVER;
+  player.alive = false;
+};
+
 const gameLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawProjectiles();
-  clearProjectiles();
+  if (currentState == GameState.PLAYING) {
+    spawnGrid();
 
-  grid.draw(ctx);
-  grid.update();
+    drawProjectiles();
+    drawParticles();
+    drawObstacles();
 
-  ctx.save();
-  ctx.translate(
-    player.position.x + player.width / 2,
-    player.position.y + player.height / 2
-  );
+    clearProjectiles();
+    clearParticles();
 
-  if (keys.shoot.pressed && keys.shoot.released) {
-    player.shoot(playerProjectiles);
-    keys.shoot.released = false;
+    shootPlayer();
+    shootInvaders();
+    shootObstacle();
+
+    grid.draw(ctx);
+    grid.update(player.alive);
+
+    ctx.save();
+
+    ctx.translate(
+      player.position.x + player.width / 2,
+      player.position.y + player.height / 2
+    );
+
+    if (keys.shoot.pressed && keys.shoot.released) {
+      player.shoot(playerProjectiles);
+      keys.shoot.released = false;
+    }
+
+    if (keys.left && player.position.x >= 0) {
+      player.moveLeft();
+      ctx.rotate(-0.15);
+    }
+    if (keys.right && player.position.x <= canvas.width - 100) {
+      player.moveRight();
+      ctx.rotate(0.15);
+    }
+
+    ctx.translate(
+      -player.position.x - player.width / 2,
+      -player.position.y - player.height / 2
+    );
+
+    player.draw(ctx);
+    ctx.restore();
   }
 
-  if (keys.left && player.position.x >= 0) {
-    player.moveLeft();
-    ctx.rotate(-0.15);
+  if (currentState == GameState.GAME_OVER) {
+    drawParticles();
+    drawProjectiles();
+    drawObstacles();
+
+    shootObstacle();
+
+    clearProjectiles();
+    clearParticles();
+
+    grid.draw(ctx);
+    grid.update(player.alive);
   }
-  if (keys.right && player.position.x <= canvas.width - 100) {
-    player.moveRight();
-    ctx.rotate(0.15);
-  }
-
-  ctx.translate(
-    -player.position.x - player.width / 2,
-    -player.position.y - player.height / 2
-  );
-
-  player.draw(ctx);
-
-  ctx.restore();
 
   requestAnimationFrame(gameLoop);
 };
@@ -104,7 +274,6 @@ addEventListener("keyup", (event) => {
 
 setInterval(() => {
   const invader = grid.getRandominvader();
-
   if (invader) {
     invader.shoot(invaderProjectiles);
   }
