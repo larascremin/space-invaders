d991962ec677c1cf4251721e565d5aac97e9002d
Lara Scremin <lscreminmendes@gmail.com>
2025-04-05 15:43:52 -0300

    Obstacle logic structure

---
+++ src/classes/Obstacle.js
@@ -0,0 +1,24 @@
+class Obstacle {
+  constructor(position, width, height, color) {
+    this.position = position;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw(ctx) {
+    ctx.fillStyl = this.color;
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  gotHit(projectile) {
+    return (
+      projectile.position.x >= this.position.x &&
+      projectile.position.x <= this.position.x + this.width &&
+      projectile.position.y >= this.position.y &&
+      projectile.position.y <= this.position.y + this.height
+    );
+  }
+}
+
+export default Obstacle;
