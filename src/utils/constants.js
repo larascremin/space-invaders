22df96c00de8aec0d7a28b360d96d93ae7ba7c67
Lara Scremin <lscreminmendes@gmail.com>
2025-04-05 15:45:07 -0300

    game state

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,9 @@ export const PATH_ENGINE_IMG = "src/assets/images/engine.png";
 export const PATH_SPRITES_IMG = "src/assets/images/sprites.png";
 export const PATH_INVADER_IMG = "src/assets/images/invader.png";
 export const INITIAL_FRAME = 10;
+
+export const GameState = {
+  START: "start",
+  PLAYING: "playing",
+  GAME_OVER: "gameOver",
+};
